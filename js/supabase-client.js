fb538841986ed28ba1389cf60b458ad2e50a9774
a-rech <alexrech@live.fr>
2026-07-17 14:06:57 +0200

    Update supabase-client.js

--- js/supabase-client.js
+++ js/supabase-client.js
@@ -7,6 +7,6 @@
 // tant que les policies RLS sont bien en place côté base.
 
 const SUPABASE_URL = "https://ypdbkivwtogzbujvoseq.supabase.co";
-const SUPABASE_ANON_KEY = "COLLEZ_VOTRE_CLE_ANON_ICI";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlwZGJraXZ3dG9nemJ1anZvc2VxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQyNzIxNjQsImV4cCI6MjA5OTg0ODE2NH0.l_Ui2szsqvsUT39uI9JUPLpzeGTYirthuBekSJb9bE0";
 
 export const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
